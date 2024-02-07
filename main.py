dcbee61cf4d5db66d88654cca49033d59b7aa82c
hieule <hieu.tg.le@gmail.com>
2024-02-07 17:33:42 -0600

    re

--- main.py
+++ main.py
@@ -11,6 +11,8 @@ import utils
 import logger
 import torch
 import argparse
+import torchvision.models as models
+import timer
 
 try:
     import poptorch
@@ -27,19 +29,28 @@ def main():
     prompt = "a shiba inu in a zen garden, acrylic painting"
     model_id = "stabilityai/stable-diffusion-2-1"
 
-    if args.device == "ipu":
-        import ipu_benchmark
+    model = models.resnet18().cuda()
+    optimizer = torch.optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+    # compiled_model = torch.compile(model, mode="max-autotune")
+    compiled_model = torch.compile(model, mode="reduce-overhead")
+    # compiled_model = model
 
-        ipu_benchmark.benchmark(prompt, model_id)
-    elif args.device == "gpu":
-        import gpu_benchmark
+    for i in range(3):
+        with timer.Timer(logger.log):
+            x = torch.randn(16, 3, 224, 224).cuda()
+            optimizer.zero_grad()
+            y = compiled_model(x)
+            y.sum().backward()
+            optimizer.step()
 
-        gpu_benchmark.benchmark(prompt, model_id)
+    # if args.device == "ipu":
+    #     import ipu_benchmark
 
-    elif args.device == "ipu_manual":
-        import manual_porting
+    #     ipu_benchmark.benchmark(prompt, model_id)
+    # elif args.device == "gpu":
+    #     import gpu_benchmark
 
-        manual_porting.benchmark(prompt, model_id)
+    #     gpu_benchmark.benchmark(prompt, model_id)
 
 
 def create_argparser() -> argparse.ArgumentParser:
