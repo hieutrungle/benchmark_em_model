431608dbe28ca94928f9c3200267678ec66e3710
hieule <hieu.tg.le@gmail.com>
2024-02-22 10:55:48 -0600

    re

--- ipu_run.sh
+++ ipu_run.sh
@@ -4,69 +4,69 @@ python main.py --device ipu --batch_size 2 --device_iterations 10 --replication_
 echo 'python main.py --device ipu --batch_size 2 --device_iterations 10 --replication_factor 1 --gradient_accumulation 20 --num_ipus 2'
 python main.py --device ipu --batch_size 2 --device_iterations 10 --replication_factor 1 --gradient_accumulation 20 --num_ipus 2
 
-# echo 'python main.py --device ipu --batch_size 2 --device_iterations 10 --replication_factor 1 --gradient_accumulation 40 --num_ipus 2'
-# python main.py --device ipu --batch_size 2 --device_iterations 10 --replication_factor 1 --gradient_accumulation 40 --num_ipus 2
+echo 'python main.py --device ipu --batch_size 2 --device_iterations 10 --replication_factor 1 --gradient_accumulation 40 --num_ipus 2'
+python main.py --device ipu --batch_size 2 --device_iterations 10 --replication_factor 1 --gradient_accumulation 40 --num_ipus 2
 
-# echo 'python main.py --device ipu --batch_size 2 --device_iterations 10 --replication_factor 1 --gradient_accumulation 60 --num_ipus 2'
-# python main.py --device ipu --batch_size 2 --device_iterations 10 --replication_factor 1 --gradient_accumulation 60 --num_ipus 2
+echo 'python main.py --device ipu --batch_size 2 --device_iterations 10 --replication_factor 1 --gradient_accumulation 60 --num_ipus 2'
+python main.py --device ipu --batch_size 2 --device_iterations 10 --replication_factor 1 --gradient_accumulation 60 --num_ipus 2
 
-# echo 'python main.py --device ipu --batch_size 2 --device_iterations 10 --replication_factor 1 --gradient_accumulation 10 --num_ipus 4'
-# python main.py --device ipu --batch_size 2 --device_iterations 10 --replication_factor 1 --gradient_accumulation 10 --num_ipus 4
+echo 'python main.py --device ipu --batch_size 2 --device_iterations 10 --replication_factor 1 --gradient_accumulation 10 --num_ipus 4'
+python main.py --device ipu --batch_size 2 --device_iterations 10 --replication_factor 1 --gradient_accumulation 10 --num_ipus 4
 
-# echo 'python main.py --device ipu --batch_size 2 --device_iterations 10 --replication_factor 1 --gradient_accumulation 20 --num_ipus 4'
-# python main.py --device ipu --batch_size 2 --device_iterations 10 --replication_factor 1 --gradient_accumulation 20 --num_ipus 4
+echo 'python main.py --device ipu --batch_size 2 --device_iterations 10 --replication_factor 1 --gradient_accumulation 20 --num_ipus 4'
+python main.py --device ipu --batch_size 2 --device_iterations 10 --replication_factor 1 --gradient_accumulation 20 --num_ipus 4
 
-# echo 'python main.py --device ipu --batch_size 2 --device_iterations 10 --replication_factor 1 --gradient_accumulation 40 --num_ipus 4'
-# python main.py --device ipu --batch_size 2 --device_iterations 10 --replication_factor 1 --gradient_accumulation 40 --num_ipus 4
+echo 'python main.py --device ipu --batch_size 2 --device_iterations 10 --replication_factor 1 --gradient_accumulation 40 --num_ipus 4'
+python main.py --device ipu --batch_size 2 --device_iterations 10 --replication_factor 1 --gradient_accumulation 40 --num_ipus 4
 
-# echo 'python main.py --device ipu --batch_size 2 --device_iterations 10 --replication_factor 1 --gradient_accumulation 60 --num_ipus 4'
-# python main.py --device ipu --batch_size 2 --device_iterations 10 --replication_factor 1 --gradient_accumulation 60 --num_ipus 4
+echo 'python main.py --device ipu --batch_size 2 --device_iterations 10 --replication_factor 1 --gradient_accumulation 60 --num_ipus 4'
+python main.py --device ipu --batch_size 2 --device_iterations 10 --replication_factor 1 --gradient_accumulation 60 --num_ipus 4
 
-# echo 'python main.py --device ipu --batch_size 2 --device_iterations 10 --replication_factor 1 --gradient_accumulation 10 --num_ipus 8'
-# python main.py --device ipu --batch_size 2 --device_iterations 10 --replication_factor 1 --gradient_accumulation 10 --num_ipus 8
+echo 'python main.py --device ipu --batch_size 2 --device_iterations 10 --replication_factor 1 --gradient_accumulation 10 --num_ipus 8'
+python main.py --device ipu --batch_size 2 --device_iterations 10 --replication_factor 1 --gradient_accumulation 10 --num_ipus 8
 
-# echo 'python main.py --device ipu --batch_size 2 --device_iterations 10 --replication_factor 1 --gradient_accumulation 20 --num_ipus 8'
-# python main.py --device ipu --batch_size 2 --device_iterations 10 --replication_factor 1 --gradient_accumulation 20 --num_ipus 8
+echo 'python main.py --device ipu --batch_size 2 --device_iterations 10 --replication_factor 1 --gradient_accumulation 20 --num_ipus 8'
+python main.py --device ipu --batch_size 2 --device_iterations 10 --replication_factor 1 --gradient_accumulation 20 --num_ipus 8
 
-# echo 'python main.py --device ipu --batch_size 2 --device_iterations 10 --replication_factor 1 --gradient_accumulation 40 --num_ipus 8'
-# python main.py --device ipu --batch_size 2 --device_iterations 10 --replication_factor 1 --gradient_accumulation 40 --num_ipus 8
+echo 'python main.py --device ipu --batch_size 2 --device_iterations 10 --replication_factor 1 --gradient_accumulation 40 --num_ipus 8'
+python main.py --device ipu --batch_size 2 --device_iterations 10 --replication_factor 1 --gradient_accumulation 40 --num_ipus 8
 
-# echo 'python main.py --device ipu --batch_size 2 --device_iterations 10 --replication_factor 1 --gradient_accumulation 60 --num_ipus 8'
-# python main.py --device ipu --batch_size 2 --device_iterations 10 --replication_factor 1 --gradient_accumulation 60 --num_ipus 8
+echo 'python main.py --device ipu --batch_size 2 --device_iterations 10 --replication_factor 1 --gradient_accumulation 60 --num_ipus 8'
+python main.py --device ipu --batch_size 2 --device_iterations 10 --replication_factor 1 --gradient_accumulation 60 --num_ipus 8
 
-# echo 'python main.py --device ipu --batch_size 2 --device_iterations 20 --replication_factor 1 --gradient_accumulation 10 --num_ipus 2'
-# python main.py --device ipu --batch_size 2 --device_iterations 20 --replication_factor 1 --gradient_accumulation 10 --num_ipus 2
+echo 'python main.py --device ipu --batch_size 2 --device_iterations 20 --replication_factor 1 --gradient_accumulation 10 --num_ipus 2'
+python main.py --device ipu --batch_size 2 --device_iterations 20 --replication_factor 1 --gradient_accumulation 10 --num_ipus 2
 
-# echo 'python main.py --device ipu --batch_size 2 --device_iterations 20 --replication_factor 1 --gradient_accumulation 20 --num_ipus 2'
-# python main.py --device ipu --batch_size 2 --device_iterations 20 --replication_factor 1 --gradient_accumulation 20 --num_ipus 2
+echo 'python main.py --device ipu --batch_size 2 --device_iterations 20 --replication_factor 1 --gradient_accumulation 20 --num_ipus 2'
+python main.py --device ipu --batch_size 2 --device_iterations 20 --replication_factor 1 --gradient_accumulation 20 --num_ipus 2
 
-# echo 'python main.py --device ipu --batch_size 2 --device_iterations 20 --replication_factor 1 --gradient_accumulation 40 --num_ipus 2'
-# python main.py --device ipu --batch_size 2 --device_iterations 20 --replication_factor 1 --gradient_accumulation 40 --num_ipus 2
+echo 'python main.py --device ipu --batch_size 2 --device_iterations 20 --replication_factor 1 --gradient_accumulation 40 --num_ipus 2'
+python main.py --device ipu --batch_size 2 --device_iterations 20 --replication_factor 1 --gradient_accumulation 40 --num_ipus 2
 
-# echo 'python main.py --device ipu --batch_size 2 --device_iterations 20 --replication_factor 1 --gradient_accumulation 60 --num_ipus 2'
-# python main.py --device ipu --batch_size 2 --device_iterations 20 --replication_factor 1 --gradient_accumulation 60 --num_ipus 2
+echo 'python main.py --device ipu --batch_size 2 --device_iterations 20 --replication_factor 1 --gradient_accumulation 60 --num_ipus 2'
+python main.py --device ipu --batch_size 2 --device_iterations 20 --replication_factor 1 --gradient_accumulation 60 --num_ipus 2
 
-# echo 'python main.py --device ipu --batch_size 2 --device_iterations 20 --replication_factor 1 --gradient_accumulation 10 --num_ipus 4'
-# python main.py --device ipu --batch_size 2 --device_iterations 20 --replication_factor 1 --gradient_accumulation 10 --num_ipus 4
+echo 'python main.py --device ipu --batch_size 2 --device_iterations 20 --replication_factor 1 --gradient_accumulation 10 --num_ipus 4'
+python main.py --device ipu --batch_size 2 --device_iterations 20 --replication_factor 1 --gradient_accumulation 10 --num_ipus 4
 
-# echo 'python main.py --device ipu --batch_size 2 --device_iterations 20 --replication_factor 1 --gradient_accumulation 20 --num_ipus 4'
-# python main.py --device ipu --batch_size 2 --device_iterations 20 --replication_factor 1 --gradient_accumulation 20 --num_ipus 4
+echo 'python main.py --device ipu --batch_size 2 --device_iterations 20 --replication_factor 1 --gradient_accumulation 20 --num_ipus 4'
+python main.py --device ipu --batch_size 2 --device_iterations 20 --replication_factor 1 --gradient_accumulation 20 --num_ipus 4
 
-# echo 'python main.py --device ipu --batch_size 2 --device_iterations 20 --replication_factor 1 --gradient_accumulation 40 --num_ipus 4'
-# python main.py --device ipu --batch_size 2 --device_iterations 20 --replication_factor 1 --gradient_accumulation 40 --num_ipus 4
+echo 'python main.py --device ipu --batch_size 2 --device_iterations 20 --replication_factor 1 --gradient_accumulation 40 --num_ipus 4'
+python main.py --device ipu --batch_size 2 --device_iterations 20 --replication_factor 1 --gradient_accumulation 40 --num_ipus 4
 
-# echo 'python main.py --device ipu --batch_size 2 --device_iterations 20 --replication_factor 1 --gradient_accumulation 60 --num_ipus 4'
-# python main.py --device ipu --batch_size 2 --device_iterations 20 --replication_factor 1 --gradient_accumulation 60 --num_ipus 4
+echo 'python main.py --device ipu --batch_size 2 --device_iterations 20 --replication_factor 1 --gradient_accumulation 60 --num_ipus 4'
+python main.py --device ipu --batch_size 2 --device_iterations 20 --replication_factor 1 --gradient_accumulation 60 --num_ipus 4
 
-# echo 'python main.py --device ipu --batch_size 2 --device_iterations 20 --replication_factor 1 --gradient_accumulation 10 --num_ipus 8'
-# python main.py --device ipu --batch_size 2 --device_iterations 20 --replication_factor 1 --gradient_accumulation 10 --num_ipus 8
+echo 'python main.py --device ipu --batch_size 2 --device_iterations 20 --replication_factor 1 --gradient_accumulation 10 --num_ipus 8'
+python main.py --device ipu --batch_size 2 --device_iterations 20 --replication_factor 1 --gradient_accumulation 10 --num_ipus 8
 
-# echo 'python main.py --device ipu --batch_size 2 --device_iterations 20 --replication_factor 1 --gradient_accumulation 20 --num_ipus 8'
-# python main.py --device ipu --batch_size 2 --device_iterations 20 --replication_factor 1 --gradient_accumulation 20 --num_ipus 8
+echo 'python main.py --device ipu --batch_size 2 --device_iterations 20 --replication_factor 1 --gradient_accumulation 20 --num_ipus 8'
+python main.py --device ipu --batch_size 2 --device_iterations 20 --replication_factor 1 --gradient_accumulation 20 --num_ipus 8
 
-# echo 'python main.py --device ipu --batch_size 2 --device_iterations 20 --replication_factor 1 --gradient_accumulation 40 --num_ipus 8'
-# python main.py --device ipu --batch_size 2 --device_iterations 20 --replication_factor 1 --gradient_accumulation 40 --num_ipus 8
+echo 'python main.py --device ipu --batch_size 2 --device_iterations 20 --replication_factor 1 --gradient_accumulation 40 --num_ipus 8'
+python main.py --device ipu --batch_size 2 --device_iterations 20 --replication_factor 1 --gradient_accumulation 40 --num_ipus 8
 
-# echo 'python main.py --device ipu --batch_size 2 --device_iterations 20 --replication_factor 1 --gradient_accumulation 60 --num_ipus 8'
-# python main.py --device ipu --batch_size 2 --device_iterations 20 --replication_factor 1 --gradient_accumulation 60 --num_ipus 8
+echo 'python main.py --device ipu --batch_size 2 --device_iterations 20 --replication_factor 1 --gradient_accumulation 60 --num_ipus 8'
+python main.py --device ipu --batch_size 2 --device_iterations 20 --replication_factor 1 --gradient_accumulation 60 --num_ipus 8
 
